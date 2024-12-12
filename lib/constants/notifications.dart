2cd502b33fd2a6825b88464d6bfbdc5384b8c0ae
aurelwidyanti <aurelwyt@gmail.com>
2024-12-13 00:50:06 +0700

    feat: Add notifications list for waste collection reminders

---
+++ lib/constants/notifications.dart
@@ -0,0 +1,50 @@
+List notifications = [
+  {
+    'title': 'Reminder',
+    'description': 'Jadwal pengambilan sampah anorganik hari ini',
+  },
+  {
+    'title': 'Reminder',
+    'description': 'Jadwal pengambilan sampah organik hari ini',
+  },
+  {
+    'title': 'Reminder',
+    'description': 'Jadwal pengambilan sampah anorganik besok',
+  },
+  {
+    'title': 'Reminder',
+    'description': 'Jadwal pengambilan sampah organik besok',
+  },
+  {
+    'title': 'Reminder',
+    'description': 'Jadwal pengambilan sampah anorganik lusa',
+  },
+  {
+    'title': 'Reminder',
+    'description': 'Jadwal pengambilan sampah organik lusa',
+  },
+  {
+    'title': 'Reminder',
+    'description': 'Jadwal pengambilan sampah anorganik minggu depan',
+  },
+  {
+    'title': 'Reminder',
+    'description': 'Jadwal pengambilan sampah organik minggu depan',
+  },
+  {
+    'title': 'Reminder',
+    'description': 'Jadwal pengambilan sampah anorganik bulan depan',
+  },
+  {
+    'title': 'Reminder',
+    'description': 'Jadwal pengambilan sampah organik bulan depan',
+  },
+  {
+    'title': 'Reminder',
+    'description': 'Jadwal pengambilan sampah anorganik tahun depan',
+  },
+  {
+    'title': 'Reminder',
+    'description': 'Jadwal pengambilan sampah organik tahun depan',
+  },
+];
