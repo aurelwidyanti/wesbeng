c4a4a8243cd52a26143704b4d1a2cbcef36b1301
aurelwidyanti <aurelwyt@gmail.com>
2024-12-13 11:02:54 +0700

    refactor: Enhance CategoryChip to support selection state with visual feedback

--- lib/widgets/category_chip.dart
+++ lib/widgets/category_chip.dart
@@ -1,20 +1,40 @@
 import 'package:flutter/material.dart';
 
-class CategoryChip extends StatelessWidget {
+class CategoryChip extends StatefulWidget {
   final String label;
 
   const CategoryChip({Key? key, required this.label}) : super(key: key);
 
   @override
+  _CategoryChipState createState() => _CategoryChipState();
+}
+
+class _CategoryChipState extends State<CategoryChip> {
+  bool _isSelected = false;
+
+  void _toggleSelection() {
+    setState(() {
+      _isSelected = !_isSelected;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Chip(
-      backgroundColor: Colors.white,
-      label: Text(label),
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(20),
-        side: BorderSide(
-          color: Colors.grey[300]!,
-          width: 1,
+    return GestureDetector(
+      onTap: _toggleSelection,
+      child: Chip(
+        backgroundColor:
+            _isSelected ? Theme.of(context).primaryColor : Colors.white,
+        label: Text(
+          widget.label,
+          style: TextStyle(color: _isSelected ? Colors.white : Colors.black),
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+          side: BorderSide(
+            color: Theme.of(context).primaryColor,
+            width: 1,
+          ),
         ),
       ),
     );
