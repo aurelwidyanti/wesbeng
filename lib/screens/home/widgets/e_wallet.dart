32c8c57e1270c257bd8e8060fc8bddb9f2505ccb
aurelwidyanti <aurelwyt@gmail.com>
2024-12-16 23:19:32 +0700

    refactor: Update EWallet widget layout with improved structure and styling

--- lib/screens/home/widgets/e_wallet.dart
+++ lib/screens/home/widgets/e_wallet.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:heroicons_flutter/heroicons_flutter.dart';
 
 class EWallet extends StatefulWidget {
@@ -16,39 +17,105 @@ class _EWalletState extends State<EWallet> {
     return Stack(
       children: [
         Positioned(
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(20),
-            child: Image.asset("assets/img/wallet.png"),
+          child: Container(
+            alignment: AlignmentDirectional.bottomStart,
+            width: double.infinity,
+            height: 200,
+            decoration: BoxDecoration(
+              borderRadius: const BorderRadius.all(Radius.circular(20)),
+              color: Theme.of(context).primaryColor,
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              child: Row(
+                children: [
+                  const Text(
+                    'E-Wallet',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const Spacer(),
+                  IconButton(
+                    icon: const Icon(
+                      HeroiconsSolid.arrowLongRight,
+                      color: Colors.white,
+                    ),
+                    onPressed: () {
+                      // Navigate to E-Wallet screen
+                    },
+                  ),
+                ],
+              ),
+            ),
           ),
         ),
-        Positioned(
-          top: 4,
-          left: 12,
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(
-                _isHidden ? '**********' : 'Rp 1.000.000',
-                style: const TextStyle(
+        Stack(
+          children: [
+            Positioned(
+              child: Container(
+                height: 144,
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(20)),
                   color: Colors.white,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey,
+                      blurRadius: 1,
+                      offset: Offset(0, 0),
+                    ),
+                  ],
                 ),
               ),
-              IconButton(
-                iconSize: 22,
-                icon: Icon(
-                  _isHidden ? HeroiconsSolid.eye : HeroiconsSolid.eyeSlash,
-                  color: Colors.white,
-                ),
-                onPressed: () {
-                  setState(() {
-                    _isHidden = !_isHidden;
-                  });
-                },
+            ),
+            Positioned(
+              top: 12,
+              left: 12,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    "Available balance",
+                    style: TextStyle(color: Colors.black, fontSize: 16),
+                  ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        _isHidden ? '**********' : 'Rp 1.500.000',
+                        style: const TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      IconButton(
+                        iconSize: 22,
+                        icon: Icon(
+                          _isHidden
+                              ? HeroiconsSolid.eye
+                              : HeroiconsSolid.eyeSlash,
+                          color: Colors.black,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            _isHidden = !_isHidden;
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+            Positioned(
+              left: 200,
+              child: Image.asset("assets/img/mascot.png", width: 160),
+            ),
+          ],
         ),
       ],
     );
