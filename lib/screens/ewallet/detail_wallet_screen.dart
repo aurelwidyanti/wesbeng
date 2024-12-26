a5e7bb93de508c498a0f32ea15d1ca2cbb079528
aurelwidyanti <aurelwyt@gmail.com>
2024-12-26 14:45:20 +0300

    refactor: Enhance DetailWalletScreen with ActionButton and improved layout

--- lib/screens/ewallet/detail_wallet_screen.dart
+++ lib/screens/ewallet/detail_wallet_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:heroicons_flutter/heroicons_flutter.dart';
+import 'package:wesbeng/screens/ewallet/widgets/action_button.dart';
 
 class DetailWalletScreen extends StatelessWidget {
   const DetailWalletScreen({super.key});
@@ -8,70 +9,102 @@ class DetailWalletScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Container(
-        height: 320,
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topRight,
-            end: Alignment.bottomLeft,
-            stops: [0.1, 0.5, 0.9],
-            colors: [
-              Color.fromRGBO(160, 214, 131, 1),
-              Color.fromRGBO(47, 81, 80, 1),
-              Colors.black,
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).primaryColor,
+        surfaceTintColor: Colors.white,
+        iconTheme: const IconThemeData(color: Colors.black),
+        title: const Text(
+          'E-Wallet',
+          style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                padding: const EdgeInsets.symmetric(vertical: 16),
+                height: 320,
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topRight,
+                    end: Alignment.bottomLeft,
+                    stops: [0.1, 0.5, 0.9],
+                    colors: [
+                      Color.fromRGBO(30, 32, 30, 1),
+                      Color.fromRGBO(60, 61, 55, 1),
+                      Color.fromRGBO(105, 117, 101, 1)
+                    ],
+                  ),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Spacer(),
+                    const Text(
+                      "Rp5000",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 36,
+                      ),
+                    ),
+                    const Spacer(),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ActionButton(
+                          iconData: HeroiconsSolid.arrowRightOnRectangle,
+                          onPressed: () {
+                            // Top up functionality
+                          },
+                        ),
+                        const SizedBox(width: 16),
+                        ActionButton(
+                          iconData: HeroiconsSolid.queueList,
+                          onPressed: () {
+                            // Withdraw functionality
+                          },
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 16),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Recent Transactions',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    ListView.builder(
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      itemCount: 5,
+                      itemBuilder: (context, index) {
+                        return Card(
+                          child: ListTile(
+                            leading: const Icon(Icons.monetization_on),
+                            title: Text('Transaction ${index + 1}'),
+                            subtitle: const Text('Details of the transaction'),
+                            trailing: const Text('-Rp1000'),
+                          ),
+                        );
+                      },
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
-        ),
-        child: Column(
-          children: [
-            //
-            Spacer(),
-            Text(
-              "Rp5000",
-              style: TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-                fontSize: 36,
-              ),
-            ),
-            Spacer(),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.white.withOpacity(0.5),
-                    shape: const CircleBorder(),
-                    padding: const EdgeInsets.all(12),
-                  ),
-                  onPressed: () {},
-                  child: const Icon(Icons.add, color: Colors.white),
-                ),
-                const SizedBox(width: 16),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.white.withOpacity(0.5),
-                    shape: const CircleBorder(),
-                    padding: const EdgeInsets.all(12),
-                  ),
-                  onPressed: () {},
-                  child: const Icon(Icons.swap_horiz, color: Colors.white),
-                ),
-                const SizedBox(width: 16),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.white.withOpacity(0.5),
-                    shape: const CircleBorder(),
-                    padding: const EdgeInsets.all(12),
-                  ),
-                  onPressed: () {},
-                  child:
-                      const Icon(Icons.list_alt_rounded, color: Colors.white),
-                ),
-              ],
-            ),
-            const SizedBox(height: 16),
-          ],
         ),
       ),
     );
